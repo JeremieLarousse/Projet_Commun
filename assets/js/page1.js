cafcefe7c72ede7b6a04baaed91b6ac6cca075f5
JeremieLarousse <97170000+JeremieLarousse@users.noreply.github.com>
2022-02-04 14:59:21 +0100

    changements

--- assets/js/page1.js
+++ assets/js/page1.js
@@ -7,53 +7,5 @@ function envoyer(){
         window.location = "../pages/page2.html"
 }
 
-// function envoyer(){
-//     let indication = document.getElementById("renseignements").value;
-//     if (typeof indication === String){
-//         window.location = "../pages/page2.html"
-//     } else {
-//     alert("Invalide"); 
-//     }
-// }
-
-// function envoyer(){
-//     // let indication = document.getElementById("renseignements").value;
-//     // if ( indication == '#'){
-//     //     alert("Invalide"); 
-//     // } else {
-//     //     window.location = "../pages/page2.html"
-//     // }
-//     window.location = "../pages/page2.html"
-// }
-
-
-
-////////////////////////////////////////////////////
-// function Soumettre(){
-//     let ToDoToDo= document.getElementById('declarer').value;
-//     let li = document.createElement('li');
-//     let a = document.createTextNode(ToDoToDo);
-//     li.appendChild(a);
-
-//     // if (ToDoToDo === ''){
-//     //     alert('vide, entrez un texte!!!')
-//     // } else {
-//     //     document.getElementById('liste').appendChild(li);  
-//     // }
-    
-//     document.getElementById('declarer').value = '';
-//     let span= document.createElement('span');
-//     // let dtxt = document.createTextNode('\u00D7');
-//     // span.className = 'del';
-//     span.appendChild(dtxt);
-//     li.appendChild(span);
-
-//     // for (let i = 0; i < del.length; i++){
-//     //     del[i].onclick = function(){
-//     //         let div = this.parentElement;          
-//     //         div.style.display = 'none';
-//     //     }
-//     // }
-// }
 
 
